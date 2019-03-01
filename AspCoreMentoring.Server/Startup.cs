7fac18500195cf5e21ee427452e96f2c78555b8a
Pavel Petrusevich <Pavel_Petrusevich@epam.com>
2019-03-01 12:27:45 +0300

    +add DI repository

--- AspCoreMentoring.Server/Startup.cs
+++ AspCoreMentoring.Server/Startup.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AspCoreMentoring.SharedInfrastructure;
 
 namespace AspCoreMentoring.Server
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json");
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.RegisterDALDependency(Configuration);
             services.AddMvc().AddNewtonsoftJson();
             services.AddResponseCompression();
         }
---
+++ AspCoreMentoring.SharedInfrastructure/DependencyInjectionExstension.cs
@@ -0,0 +1,25 @@
+﻿using AspCoreMentoring.DAL.Common.Interfaces;
+using AspCoreMentoring.DAL.DbContext;
+using AspCoreMentoring.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspCoreMentoring.SharedInfrastructure
+{
+    public static class DependencyInjectionExstension
+    {
+        public static void RegisterDALDependency(this IServiceCollection serviceCollection,IConfiguration configuration)
+        {
+            serviceCollection.AddDbContext<NorthWindContext>(option => {
+                option.UseSqlServer(configuration.GetConnectionString("NorthwindDb"));
+            });
+            serviceCollection.AddScoped<IProductRepository, ProductRepository>();
+            serviceCollection.AddScoped<ICategoryRepository, CategoryRepository>();
+            serviceCollection.AddScoped<ISupplierRepository, SupplierRepository>();
+        }
+    }
+}
