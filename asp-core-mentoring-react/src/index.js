2961560e99f0434b39a8c1e765cdce8c9669317c
Pavel Petrusevich <Pavel_Petrusevich@epam.com>
2019-03-15 16:06:58 +0300

    +

--- asp-core-mentoring-react/src/index.js
+++ asp-core-mentoring-react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/root/App';
+import App from './App/GeneralLayout/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
